63dff55306d256d29c496f2f1db2cb4dd76c86d0
Amr Salah <amrs22131@gmail.com>
2022-06-18 23:58:52 +0200

    modified setup

--- JS/setup.js
+++ JS/setup.js
@@ -3,6 +3,9 @@ if(localStorage.length == 0){
     {username:"David",email:"D@D",password:"D"},{username:"Emad",email:"E@E",password:"E"},{username:"Farid",email:"F@F",password:"F"},
     {username:"Gavir",email:"G@G",password:"G"},{username:"Hassan",email:"H@H",password:"H"},{username:"Islam",email:"I@I",password:"I"},
     {username:"admin",email:"admin@admin",password:"admin",isAdmin:true}]
+    let books_data = [{title:"lord of the rings",author:"stephen king",desccription:"great book",img:"../images/book1.png",id:"0"},
+    {title:"hobbit",author:"stephen queen",desccription:"good book",img:"../images/book2.png",id:"1"}]
     localStorage.setItem('users',JSON.stringify(user_data))
+    localStorage.setItem('books',JSON.stringify(books_data))
 }
 
