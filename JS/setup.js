1cc38efb978b10cf72fbfbc4777a9dc5eca12ed4
zyad yasser <zyadyasser626@gmail.com>
2022-06-19 01:23:28 +0200

    changed the setup file condition

--- JS/setup.js
+++ JS/setup.js
@@ -1,4 +1,4 @@
-if(localStorage.length == 0){
+if(localStorage.getItem("users") == null || localStorage.getItem("books") == null){
     let user_data = [{username:"Ahmed",email:"A@A",password:"A"},{username:"Bassem",email:"B@B",password:"B"},{username:"Camel",email:"C@C",password:"C"},
     {username:"David",email:"D@D",password:"D"},{username:"Emad",email:"E@E",password:"E"},{username:"Farid",email:"F@F",password:"F"},
     {username:"Gavir",email:"G@G",password:"G"},{username:"Hassan",email:"H@H",password:"H"},{username:"Islam",email:"I@I",password:"I"},
