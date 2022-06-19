d7dd1c35e18c45686e22e532fdfeb9a97cf3ca78
zyad yasser <zyadyasser626@gmail.com>
2022-06-19 17:27:30 +0200

    login/signup links now change to username & icon after logging-in and vice-versa

--- JS/login.js
+++ JS/login.js
@@ -8,7 +8,7 @@ function check_data(){
         localStorage.setItem("current_user", JSON.stringify(USERS1[i]))
         localStorage.setItem("session_type", "user")
         if(USERS1[i].isAdmin)localStorage.setItem("session_type", "admin")
-        window.open("../html/home.html", "_self")
+        window.open("../html/index.html", "_self")
         return
       }
     }
--- JS/setup.js
+++ JS/setup.js
@@ -10,8 +10,10 @@ if(localStorage.getItem("users") == null || localStorage.getItem("books") == nul
     {username:"Iman",email:"Iman@gmail.com",password:"I",isAdmin:false,id:"8",img:"../images/user.png"},
     {username:"admin",email:"admin@admin.com",password:"admin",isAdmin:true,id:"9",img:"../images/icons8-male-user-96.png"}]
 
-    let books_data = [{title:"lord of the rings",author:"stephen king",description:"great book",img:"../images/book1.png",id:"0"},
-    {title:"hobbit",author:"stephen queen",description:"good book",img:"../images/book2.png",id:"1"}]
+    let books_data = [
+        {title:"lord of the rings",author:"stephen king",description:"great book",img:"../images/book1.png",id:"0"},
+        {title:"hobbit",author:"stephen queen",description:"good book",img:"../images/book2.png",id:"1"}
+]
     localStorage.setItem('users',JSON.stringify(user_data))
     localStorage.setItem('books',JSON.stringify(books_data))
 }
@@ -19,7 +21,38 @@ if(localStorage.getItem("users") == null || localStorage.getItem("books") == nul
 if(localStorage.getItem("session_type") == "user")
     document.querySelectorAll(".for-admin").forEach((element)=>element.style.display = "none")
 
-let current_user = JSON.parse(localStorage.getItem("current_user"))
-let info_element = document.querySelector(".icon-div")
-let user_name = info_element.querySelector("h3")
- user_name.textContent= current_user.username
+if(localStorage.getItem("current_user") != null){
+    let current_user = JSON.parse(localStorage.getItem("current_user"))
+    
+    let info_element = document.createElement("div")
+    let user_name = document.createElement("h3")
+    let icon_element = document.createElement("a")
+    let img = document.createElement("img")
+
+    info_element.setAttribute("class", "icon-div")
+    img.setAttribute("src", "../images/icons8-male-user-96.png")
+
+    info_element.appendChild(user_name)
+    info_element.appendChild(icon_element)
+    icon_element.appendChild(img)
+    document.querySelector("header").appendChild(info_element)
+
+    user_name.textContent= current_user.username
+}
+else{
+    let links_div = document.createElement("div")
+    let login_link = document.createElement("a")
+    let signup_link = document.createElement("a")
+
+    links_div.setAttribute("class", "header-right")
+    login_link.setAttribute("href", "login.html")
+    signup_link.setAttribute("href", "Signup.html")
+
+    login_link.textContent = "Login"
+    signup_link.textContent = "Sign Up"
+
+    links_div.appendChild(login_link)
+    links_div.appendChild(signup_link)
+    document.querySelector("header").appendChild(links_div)
+}
+
