7aed053e45ad041c9f0570eb9705fd87a600d266
Amr Salah <amrs22131@gmail.com>
2022-06-19 07:44:54 +0200

    added delete and edit books functionalities

--- JS/bookdetails.js
+++ JS/bookdetails.js
@@ -6,9 +6,62 @@ let Books = JSON.parse(localStorage.getItem('books'))
 window.onload = function (){
     var parameters = new URLSearchParams(window.location.search)
     var bookIndex = parameters.get("index")
-    Title.innerHTML= Books[bookIndex].title
-    bookDescription.innerHTML = Books[bookIndex].description
+    Title.textContent= Books[bookIndex].title
+    bookDescription.textContent = Books[bookIndex].description
     bookImage.setAttribute('src',Books[bookIndex].img)
-    Author.innerHTML = "Author: "+Books[bookIndex].author
+    Author.textContent = "Author: "+Books[bookIndex].author
 }
 
+let titleChange = document.getElementById('titleMod')
+let descriptionChange = document.getElementById('descriptionMod')
+let authorChange = document.getElementById('authorMod')
+
+function openEditForm() {
+    document.getElementById("editForm").style.display = "block";
+    titleChange.value = Title.textContent
+    descriptionChange.value = bookDescription.textContent
+    authorChange.value = Author.textContent.replace("Author: ","")
+  }
+function submitEditForm() {
+    document.getElementById("editForm").style.display = "none";
+    Title.textContent= titleChange.value
+    bookDescription.textContent = descriptionChange.value
+    Author.textContent = "Author: "+ authorChange.value
+    var parameters = new URLSearchParams(window.location.search)
+    var bookIndex = parameters.get("index")
+    Books[bookIndex].title = Title.textContent
+    Books[bookIndex].description = bookDescription.textContent
+    Books[bookIndex].author = Author.textContent.replace("Author: ","")
+    localStorage.setItem('books',JSON.stringify(Books))
+  }
+  function closeEditForm() {
+    document.getElementById("editForm").style.display = "none";
+}
+  
+
+
+  let adminPassword = document.getElementById('adminPassword')
+  function openDeleteForm() {
+    document.getElementById("deleteForm").style.display = "block";
+    titleChange.value = Title.textContent
+    descriptionChange.value = bookDescription.textContent
+    authorChange.value = Author.textContent.replace("Author: ","")
+  }
+function submitDeleteForm() {
+    document.getElementById("deleteForm").style.display = "none";
+    if(adminPassword.value == "admin"){
+        var parameters = new URLSearchParams(window.location.search)
+        var bookIndex = parameters.get("index") 
+        Books.splice(bookIndex,1)
+        for(i=bookIndex;i<Books.length;i++){
+            Books[i].id -= `${1}` ;
+        }
+        localStorage.setItem('books',JSON.stringify(Books))
+        window.open("../html/All_Books.html","_self")
+    }
+    else{
+        window.alert("wrong password")
+    }
+  }
+  function closeDeleteForm() {
+    document.getElementById("deleteForm").style.display = "none";}
