e41eba39141c379912074bdd33e2c872f7ffc37c
zyad yasser <zyadyasser626@gmail.com>
2022-06-20 17:32:48 +0200

    added the best-seller-checkbox logix

--- JS/bookdetails.js
+++ JS/bookdetails.js
@@ -2,15 +2,19 @@ var Title = document.getElementById('title')
 var bookDescription= document.getElementById('bookDescription')
 var bookImage= document.getElementById('bookImage')
 var Author = document.getElementById('Author')
-let Books = JSON.parse(localStorage.getItem('books'))
+let best_seller_btn = document.getElementById("best-seller-btn")
+
+
 let Users = JSON.parse(localStorage.getItem('users'))
 window.onload = function (){
+    let Books = JSON.parse(localStorage.getItem('books'))
     var parameters = new URLSearchParams(window.location.search)
     var bookIndex = parameters.get("index")
     Title.textContent= Books[bookIndex].title
     bookDescription.textContent = Books[bookIndex].description
     bookImage.setAttribute('src',Books[bookIndex].img)
     Author.textContent = "Author: "+Books[bookIndex].author
+    best_seller_btn.checked = Books[bookIndex].isBestSeller
 
     var todaysDate = new Date();
     var year = todaysDate.getFullYear();                        
@@ -36,6 +40,7 @@ function openEditForm() {
     authorChange.value = Author.textContent.replace("Author: ","")
   }
 function submitEditForm() {
+    let Books = JSON.parse(localStorage.getItem('books'))
     document.getElementById("editForm").style.display = "none";
     Title.textContent= titleChange.value
     bookDescription.textContent = descriptionChange.value
@@ -64,6 +69,7 @@ function submitEditForm() {
     authorChange.value = Author.textContent.replace("Author: ","")
   }
 function submitDeleteForm() {
+  let Books = JSON.parse(localStorage.getItem('books'))
     document.getElementById("deleteForm").style.display = "none";
     if(adminPassword.value == "admin"){
         var parameters = new URLSearchParams(window.location.search)
@@ -79,92 +85,111 @@ function submitDeleteForm() {
         window.alert("wrong password")
     }
   }
-  function closeDeleteForm() {
-    document.getElementById("deleteForm").style.display = "none";}
+function closeDeleteForm() {
+  document.getElementById("deleteForm").style.display = "none";
+}
 
 
-    //Borrow Form
-    let pickupDate = document.getElementById('pickupDate')
-    let returnDate = document.getElementById('returnDate')
-    function openBorrowForm() {
+//Borrow Form
+let pickupDate = document.getElementById('pickupDate')
+let returnDate = document.getElementById('returnDate')
+function openBorrowForm() {
+  let Books = JSON.parse(localStorage.getItem('books'))
+  var parameters = new URLSearchParams(window.location.search)
+  var bookIndex = parameters.get("index")
+  if(localStorage.getItem("current_user") != null){
+    if(Books[bookIndex].isBorrowed == true){
+      window.alert("Book is out of stock and will be avaliable at " + Books[bookIndex].available)
+      return
+    }
+    document.getElementById("borrowForm").style.display = "block";
+  }
+  else{
+    window.open("../html/SignUp.html")
+  }
+}
+  
+function submitBorrowForm() {
+  let Books = JSON.parse(localStorage.getItem('books'))
+    let date = new Date()
+    if(pickupDate.value == returnDate.value){
+      window.alert("borrow period of minimum one day")
+      return
+    }
+    else if(pickupDate.value > returnDate.value){
+      window.alert("return date should be after pickup date")
+      return
+    }
+      document.getElementById("borrowForm").style.display = "none";
+      let userId = JSON.parse(localStorage.getItem('current_user'))
       var parameters = new URLSearchParams(window.location.search)
       var bookIndex = parameters.get("index")
-      if(localStorage.getItem("current_user") != null){
-        if(Books[bookIndex].isBorrowed == true){
-          window.alert("Book is out of stock and will be avaliable at " + Books[bookIndex].available)
-          return
-        }
-        document.getElementById("borrowForm").style.display = "block";
-      }
-      else{
-        window.open("../html/SignUp.html")
-      }
-    }
-  
-  function submitBorrowForm() {
-      let date = new Date()
-      if(pickupDate.value == returnDate.value){
-        window.alert("borrow period of minimum one day")
-        return
-      }
-      else if(pickupDate.value > returnDate.value){
-        window.alert("return date should be after pickup date")
-        return
-      }
-        document.getElementById("borrowForm").style.display = "none";
-        let userId = JSON.parse(localStorage.getItem('current_user'))
-        var parameters = new URLSearchParams(window.location.search)
-        var bookIndex = parameters.get("index")
-        Books[bookIndex].borrowUser = userId.id
-        Books[bookIndex].borrowed = pickupDate.value
-        Books[bookIndex].available = returnDate.value
-        Books[bookIndex].isBorrowed = true
-        localStorage.setItem('books',JSON.stringify(Books))
-        if(!Users[userId.id].borrowedBooks){
-          Users[userId.id].borrowedBooks = Books[bookIndex].title
-          Users[userId.id].numberOfBorrowedBooks = 1
-        }
-        else{
-          Users[userId.id].borrowedBooks += ","+Books[bookIndex].title
-          Users[userId.id].numberOfBorrowedBooks += 1
-        }
-        localStorage.setItem('users',JSON.stringify(Users))
-    }
-    function closeBorrowForm() {
-      document.getElementById("borrowForm").style.display = "none";}
-
-  //admin return book
-  function bookAvailable(){
-    var parameters = new URLSearchParams(window.location.search)
-    var bookIndex = parameters.get("index")
-    if(Books[bookIndex].isBorrowed == true){
-      Books[bookIndex].isBorrowed = false
+      Books[bookIndex].borrowUser = userId.id
+      Books[bookIndex].borrowed = pickupDate.value
+      Books[bookIndex].available = returnDate.value
+      Books[bookIndex].isBorrowed = true
       localStorage.setItem('books',JSON.stringify(Books))
-      let i = Books[bookIndex].borrowUser
-      let number = Users[i].numberOfBorrowedBooks
-      if(number>1){
-        console.log(Users[i].borrowedBooks)
-        let titles = Users[i].borrowedBooks.split(',')
-        for(j =0; j<titles.length; j++){
-          if(titles[j] == Books[bookIndex].title){
-            titles.splice(j,1)
-            titles.join()
-            Users[i].borrowedBooks = titles.toString()
-            Users[i].numberOfBorrowedBooks -= 1
-            localStorage.setItem('users',JSON.stringify(Users))
-            window.alert("Successful! "+Books[bookIndex].title+" is avaliable to borrow now")
-            break;
-          } 
-        }
+      if(!Users[userId.id].borrowedBooks){
+        Users[userId.id].borrowedBooks = Books[bookIndex].title
+        Users[userId.id].numberOfBorrowedBooks = 1
       }
       else{
-        Users[i].numberOfBorrowedBooks -= 1
-        delete Users[i].borrowedBooks;
-        localStorage.setItem('users',JSON.stringify(Users))
-        window.alert("Successful! "+Books[bookIndex].title+" is avaliable to borrow now")
+        Users[userId.id].borrowedBooks += ","+Books[bookIndex].title
+        Users[userId.id].numberOfBorrowedBooks += 1
+      }
+      localStorage.setItem('users',JSON.stringify(Users))
+}
+
+function closeBorrowForm() {
+  document.getElementById("borrowForm").style.display = "none";
+}
+
+//admin return book
+function bookAvailable(){
+  let Books = JSON.parse(localStorage.getItem('books'))
+  var parameters = new URLSearchParams(window.location.search)
+  var bookIndex = parameters.get("index")
+  if(Books[bookIndex].isBorrowed == true){
+    Books[bookIndex].isBorrowed = false
+    localStorage.setItem('books',JSON.stringify(Books))
+    let i = Books[bookIndex].borrowUser
+    let number = Users[i].numberOfBorrowedBooks
+    if(number>1){
+      console.log(Users[i].borrowedBooks)
+      let titles = Users[i].borrowedBooks.split(',')
+      for(j =0; j<titles.length; j++){
+        if(titles[j] == Books[bookIndex].title){
+          titles.splice(j,1)
+          titles.join()
+          Users[i].borrowedBooks = titles.toString()
+          Users[i].numberOfBorrowedBooks -= 1
+          localStorage.setItem('users',JSON.stringify(Users))
+          window.alert("Successful! "+Books[bookIndex].title+" is avaliable to borrow now")
+          break;
+        } 
       }
     }
     else{
-      window.alert("book is already avaliable")
+      Users[i].numberOfBorrowedBooks -= 1
+      delete Users[i].borrowedBooks;
+      localStorage.setItem('users',JSON.stringify(Users))
+      window.alert("Successful! "+Books[bookIndex].title+" is avaliable to borrow now")
     }
   }
+  else{
+    window.alert("book is already avaliable")
+  }
+}
+
+//best-seller checkbox
+best_seller_btn.addEventListener("change", (e)=>{
+  let parameters = new URLSearchParams(window.location.search)
+  let bookIndex = parameters.get("index")
+  let books = JSON.parse(localStorage.getItem("books"))
+  if(e.target.checked){
+    books[bookIndex].isBestSeller = true
+  }else{
+    books[bookIndex].isBestSeller = false
+  }
+  localStorage.setItem("books", JSON.stringify(books))
+})
